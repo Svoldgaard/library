937066dd71f7ca6f28d576ca3c8b12925592c465
Svoldgaard <Svoldgaard96@gmail.com>
2025-09-15 11:57:43 +0200

    changes to the gitignore and appsetting file

--- server/api/Program.cs
+++ server/api/Program.cs
@@ -1,4 +1,13 @@
+using Infrastructure.Postgres.Scaffolding;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<MyDbContext>(conf =>
+{
+    conf.UseNpgsql();
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
