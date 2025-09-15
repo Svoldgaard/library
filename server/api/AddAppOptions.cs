d6465488fe5bcab447abb0bb40b0c267eea20cff
Svoldgaard <Svoldgaard96@gmail.com>
2025-09-15 12:11:59 +0200

    AppOptions created to get a more secure way to have dbstring

---
+++ server/api/AddAppOptions.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace api;
+
+public static class AppOptionsExtensions
+{
+    public static AppOptions AddAppOptions(this IServiceCollection services, IConfiguration configuration)
+    {
+        var appOptions = new AppOptions();
+        configuration.GetSection(nameof(AppOptions)).Bind(appOptions);
+
+        services.Configure<AppOptions>(configuration.GetSection(nameof(AppOptions)));
+
+        ICollection<ValidationResult> results = new List<ValidationResult>();
+        var validated = Validator.TryValidateObject(appOptions, new ValidationContext(appOptions), results, true);
+        if (!validated)
+            throw new Exception(
+                $"{string.Join(", ", results.Select(r => r.ErrorMessage))}");
+
+        return appOptions;
+    }
+}
