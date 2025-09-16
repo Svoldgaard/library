ec748d4c037f719d33bc0077cb5975d7e12a0f5b
Svoldgaard <Svoldgaard96@gmail.com>
2025-09-16 10:12:33 +0200

    architecture & classes for crud

---
+++ server/api/Seeder.cs
@@ -0,0 +1,49 @@
+using dataccess;
+using efscaffold.Entities;
+using Infrastructure.Postgres.Scaffolding;
+
+namespace api.Etc;
+
+public class Seeder(MyDbContext ctx) : ISeeder
+{
+    public async Task Seed()
+    {   
+        //Remove all before starting to add any
+        ctx.Books.RemoveRange(ctx.Books);
+        ctx.Authors.RemoveRange(ctx.Authors);
+        ctx.Genres.RemoveRange(ctx.Genres);
+        ctx.SaveChanges();
+        
+        var author = new Author()
+        {
+            Createdat = DateTime.UtcNow,
+            Id = Guid.NewGuid().ToString(),
+            Name = "Bob"
+        };
+        ctx.Authors.Add(author);
+        ctx.SaveChanges();
+        var book = new Book()
+        {
+            Createdat = DateTime.UtcNow,
+            Id = Guid.NewGuid().ToString(),
+            Pages = 42,
+            Title = "Bobs book"
+        };
+        ctx.Books.Add(book);
+        ctx.SaveChanges();
+        var genre = new Genre()
+        {
+            Createdat = DateTime.UtcNow,
+            Id = Guid.NewGuid().ToString(),
+            Name = "thriller"
+        };
+        ctx.Genres.Add(genre);
+        ctx.SaveChanges();
+        
+    }
+}
+
+public interface ISeeder
+{
+    public Task Seed();
+}
